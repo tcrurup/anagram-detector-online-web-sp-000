b78ba88906e11de4ddc9681894ece7322d9c4bb0
Anthony Rurup <tcrurup@gmail.com>
2019-05-01 02:25:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(initial_word)
+    @word = initial_word
+  end
+  
+  def match(array_to_match)
+    
+  end
+end
