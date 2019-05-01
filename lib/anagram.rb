d1be5b612cb86cf15228cdfa557b6ff27951c2a7
Anthony Rurup <tcrurup@gmail.com>
2019-05-01 02:32:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,21 @@
 class Anagram
   
-  attr_accessor :word 
+  attr_accessor :target_word 
   
   def initialize(initial_word)
-    @word = initial_word
+    @target_word = initial_word
   end
   
   def match(words_array)
-    words_array.detect{ |word| word.split("").sort === self.word.split("").sort }
+    
+    matched_anagrams = []
+    
+    words_array.each do |word| 
+      if word.split("").sort === self.target_word.split("").sort 
+        matched_anagrams << word
+      end
+    end
+    
+    matched_anagrams
   end
 end
